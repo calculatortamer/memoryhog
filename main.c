9895ce3feab050a718a151d8cf841362e4264ca8
calculatortamer <87210024+calculatortamer@users.noreply.github.com>
2021-07-10 01:41:25 +0200

    replace main.c
    forget about the old thing, this one is the real one

--- main.c
+++ main.c
@@ -1,26 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+unsigned long string_to_long(char* input);
 
 int main(int argc, char *argv[]){
-	if(argc<2){
-		printf("cant hog 0 memory, give me more\n");
-		return 1;
+	char* memory_to_eat=malloc(20);
+	if(argc<2){ //if user didnt give argument
+		printf("how much memory can i eat ?\n");
+		fgets(memory_to_eat,20,stdin); //20 is the maximum letters required to fit a long value in a string
 	}
-	unsigned long memorysize=atol(argv[1]);
+	else{
+		memory_to_eat=argv[1];
+	}
+	
+	unsigned long memorysize = string_to_long(memory_to_eat); //converting string to long
 	printf("allocating %lu bytes of memory\n",memorysize);
-	char* memoryallocated=malloc(memorysize); //allocating memory
-	if(memoryallocated==NULL){
+	char* memoryallocated = malloc(memorysize); //allocating memory
+	
+	
+	if(memoryallocated == NULL){ //if malloc returned null, it means you are too greedy/system has not that much RAM
 		printf("malloc failed !!\n");
 	}
 	else{
 		printf("malloc success\n");
 	}
-	printf("writing to memory");
-	for(unsigned long  i=0;i<memorysize;i++){
-		memoryallocated[i]=(char)rand();
+	
+	
+	printf("writing to memory\n");
+	for(unsigned long  i = 0 ; i<memorysize ;i++){
+		memoryallocated[i] = (char)rand(); //putting random characters in memory incase ram is compressed or unused
 	}
-	printf("writing done");
-	gets(); //wait until user is satisfied
+	printf("writing done\n");
+	
+	getchar(); //wait until user is satisfied
+	free(memoryallocated);
 	return 0;
+}
+
+
+unsigned long string_to_long(char* input){
+	unsigned long output=0;
+	unsigned int i=0;
+	while(input[i+1]!='\x00'){
+		output*=10; //decimalshift to the left
+		output+=input[i]-48; //48 is the position of '0' in ASCII, '0' - 48 = 0 | '9' - 48 = 9
+		i++;
+	}
+	return output;
 }
