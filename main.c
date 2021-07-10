7620e03b8866520b7f31494d8848571313dd7664
calculatortamer <87210024+calculatortamer@users.noreply.github.com>
2021-07-10 03:06:53 +0200

    version 1.1.0
    fixed weird bugs (i hate you, whoever said fgets is better than gets)
    added unit suffixes, you can say now 4G instead of 4000000000

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]){
 	char* memory_to_eat=malloc(20);
 	if(argc<2){ //if user didnt give argument
 		printf("how much memory can i eat ?\n");
-		fgets(memory_to_eat,20,stdin); //20 is the maximum letters required to fit a long value in a string
+		scanf("%s",memory_to_eat);
 	}
 	else{
 		memory_to_eat=argv[1];
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]){
 	
 	
 	printf("writing to memory\n");
-	for(unsigned long  i = 0 ; i<memorysize ;i++){
+	for(unsigned long  i=0;i<memorysize;i++){
 		memoryallocated[i] = (char)rand(); //putting random characters in memory incase ram is compressed or unused
 	}
 	printf("writing done\n");
@@ -41,9 +41,48 @@ int main(int argc, char *argv[]){
 unsigned long string_to_long(char* input){
 	unsigned long output=0;
 	unsigned int i=0;
-	while(input[i+1]!='\x00'){
-		output*=10; //decimalshift to the left
-		output+=input[i]-48; //48 is the position of '0' in ASCII, '0' - 48 = 0 | '9' - 48 = 9
+	int unit=0; //empty=0, kilo=1, mega=2, giga=3, etc...
+	int multiplier=0; //either 1000 or 1024
+	while(input[i]!='\x00'){
+		switch(input[i]){
+		case 'k':
+			unit=1;
+			break;
+		case 'M':
+			unit=2;
+			break;
+		case 'G':
+			unit=3;
+			break;
+		case 'T':
+			unit=4;
+			break;
+		case 'P':
+			unit=5;
+			break;
+		case 'E':
+			unit=6;
+			break;
+		case 'i':
+			break;
+		default:
+			output*=10; //decimalshift to the left
+			output+=input[i]-48; //48 is the position of '0' in ASCII, '0' - 48 = 0 | '9' - 48 = 9
+		}
+		if(unit!=0){
+			if(input[i+1]=='i'){ //checking for an 'i' to see if we use 1000 or 1024
+				multiplier=1024;
+			}
+			else{
+				multiplier=1000;
+			}
+			for(unsigned int j=0;j<unit;j++){
+				output*=multiplier;
+			}
+		}
+		if(unit){//if it detected an unit, stop the loop
+			break;
+		}
 		i++;
 	}
 	return output;
